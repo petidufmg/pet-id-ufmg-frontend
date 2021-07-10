0e80a319465b89b75ea3458eba438ca06b6ec3d2
Henrique Barbosa <henriqbarbosa@icloud.com>
2021-07-10 16:20:08 -0300

    Change create user controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,30 @@
 import User from "../models/user.js";
 
 const createUser = (req, res) => {
-  console.log("created user");
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    name: req.body.name,
+    address: req.body.address,
+    institution: req.body.institution,
+    type: req.body.type
+  });
+  User.findOne({username: req.body.username}, (err, currentUser) => {
+    if (err) {
+      res.status(500).json();
+    } else if (currentUser) {
+      res.status(404).json({error: "User already exists"});
+    } else {
+      user.save((savedErr, savedUser) => {
+        if (savedErr) {
+          res.status(500).json();
+        } else {
+          res.status(200).json({id: savedUser.id});
+        }
+      });
+    }
+  });
 };
 
 const getUser = (req, res) => {
