c9b1d6f3401c2e02a1ec6af8b920739cb46dc29b
Henrique Batista Barbosa <henriquebarbosa@ioasys.com.br>
2021-08-23 21:43:31 -0300

    Add image upload to pet-add

--- frontend/src/pages/PetAdd.js
+++ frontend/src/pages/PetAdd.js
@@ -3,6 +3,7 @@ import PetEnum from "../attributes/pet/Pet";
 import OwnerEnum from "../attributes/owner/Owner";
 import PetAddStyles from "../styles/hooks/petAddStyles";
 import addPetFormSwitcher from "../helpers/addPetFormSwitcher";
+import ImageUploader from "react-images-upload";
 import { useState } from "react";
 import { useLocation, useHistory } from "react-router-dom";
 
@@ -10,8 +11,11 @@ function PetAdd(props) {
   const petAddClasses = PetAddStyles();
   const location = useLocation();
   const history = useHistory();
+  const defaultImageLabel = "Tamanho máximo do arquivo: 5mb, formatos: jpg, gif, png";
+  const [imageLabel, setImageLabel] = useState(defaultImageLabel);
   const [form, setForm] = useState(
     location.state || {
+      image: [],
       radio: "male",
       dates: {},
       date: {},
@@ -26,11 +30,28 @@ function PetAdd(props) {
     }
   }
 
+  function handleOnDrop(picture) {
+    setImageLabel(picture[0] ? picture[0].name : defaultImageLabel);
+    setForm((prev) => ({
+      ...prev,
+      image: picture
+    }));
+  }
+
   return (
     <div className={petAddClasses.root}>
       <form>
         <h2 align="center">Informações do animal</h2>
         <Grid container spacing="5">
+          <Grid xs={12} item>
+            <ImageUploader
+              buttonText="Escolher foto do animal"
+              label={imageLabel}
+              singleImage={true}
+              onChange={handleOnDrop}
+              withPreview={true}
+            />
+          </Grid>
           {Object.values(PetEnum).map((pet, index) => (
             <Grid item spacing="5" md={6} xs={12} align="center">
               {addPetFormSwitcher(pet, form, setForm, index)}
